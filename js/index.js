7541314e297b5c91e971bf7a54d1f5263afd1890
蒙世裕 <1415940875@qq.com>
2018-04-26 11:02:45 +0800

    图片问题4

--- js/index.js
+++ js/index.js
@@ -4,8 +4,6 @@ app.directive("myList",function(){
 	return {
 		link:function(scope,element,attrs,rootScope){
 			element.on("click",function(){
-				//动态加载 CSS 文件
-				dynamicLoading.css("css/index.css");
 				//清除背景
 				element.parent().parent().children().children().removeClass("back");
 				//添加背景
@@ -127,26 +125,3 @@ app.config([
 		)
 	}
 ]);
-var dynamicLoading = {
-  css: function(path){
- if(!path || path.length === 0){
-  throw new Error('argument "path" is required !');
- }
- var head = document.getElementsByTagName('head')[0];
-    var link = document.createElement('link');
-    link.href = path;
-    link.rel = 'stylesheet';
-    link.type = 'text/css';
-    head.appendChild(link);
-  },
-  js: function(path){
- if(!path || path.length === 0){
-  throw new Error('argument "path" is required !');
- }
- var head = document.getElementsByTagName('head')[0];
-    var script = document.createElement('script');
-    script.src = path;
-    script.type = 'text/javascript';
-    head.appendChild(script);
-  }
-}
